3542043fc9b01d79aafc3bb7c9c2995b52026abf
Aaron Iniguez <aaron.iniguez@vrtcal.com>
2019-11-03 01:24:21 -0700

    [Fix] added another leet code question

---
+++ subtree-another-tree.js
@@ -0,0 +1,17 @@
+//572. Subtree of Another Tree
+//https://leetcode.com/problems/subtree-of-another-tree/
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} s
+ * @param {TreeNode} t
+ * @return {boolean}
+ */
+var isSubtree = function(s, t) {
+    
+};
