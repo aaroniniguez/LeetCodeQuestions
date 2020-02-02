844979e82e1110955fe7f69f38fb42aaa4109038
Aaron Iniguez <aaron.iniguez@vrtcal.com>
2020-02-01 17:27:16 -0800

    WIP

--- cloneGraph.js
+++ cloneGraph.js
@@ -1,14 +1,40 @@
-/**
- * // Definition for a Node.
- * function Node(val, neighbors) {
- *    this.val = val === undefined ? 0 : val;
- *    this.neighbors = neighbors === undefined ? [] : neighbors;
- * };
- */
+// Definition for a Node.
+function Node(val, neighbors) {
+   this.val = val === undefined ? 0 : val;
+   this.neighbors = neighbors === undefined ? [] : neighbors;
+};
 /**
  * @param {Node} node
  * @return {Node}
  */
+
+ //dictionary with node index being the node number
+var cloneNode = function(node, copyNodes) {
+	//if it already exists, dont need to create it...
+	if(copyNodes[node.val]) {
+		return copyNodes[node.val];
+	}
+	else
+		copyNodes[node.val] = new Node(node.val);
+	//copy neighbors
+	let neighbors = node.neighbors;
+	let clonedNeighbors = []
+	neighbors.forEach((neighborNode) => {
+		clonedNeighbors.push(cloneNode(neighborNode, copyNodes));
+	});
+	//save copy
+	copyNodes[node.val].neighbors = clonedNeighbors;
+	return copyNodes[node.val];
+}
 var cloneGraph = function(node) {
-    
+	if(node === null)
+		return node
+	let copyNodes = []; 
+	let copyNode = cloneNode(node, copyNodes);
+	return copyNode;
 };
+//let node3 = new Node(3)
+//let node5 = new Node(5)
+//let node1 = new Node(1, [node3, node5]);
+
+//cloneGraph(node1)
